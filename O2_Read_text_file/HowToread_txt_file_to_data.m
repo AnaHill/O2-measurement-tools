b96ddb2ab32cab6c4909d21e60f8159620641a9a
Antti Mäki <aj_maki@hotmail.com>
2022-02-11 12:50:57 +0200

    How to read text file added.

--- O2_Read_text_file/HowToread_txt_file_to_data.m
+++ O2_Read_text_file/HowToread_txt_file_to_data.m
@@ -1,11 +1,13 @@
-%% Kuinka lukea O2 data jos se tekstiedostossa (esim kaatunut, jolloin ei .mat)
+%% How to read O2 data from text file, if e.g. crashed without saving to .mat file 
+% Kuinka lukea O2 data jos se tekstiedostossa (esim jos kaatunut, jolloin ei .mat)
 
-%% Luetaan data: without start & end Rows
+%% Reading data: 
+% without start & end Rows
 clear, [t_,phiM_,ampM_,pO2M_] = O2_import_measurement_data_from_text_file();
 % with specific start & end rows
 % [t_,phiM_,ampM_,pO2M_] = O2_import_measurement_data_from_text_file(startRow, endRow)
 
-%% Kysyt‰‰n tallennuspaikka ja nimi
+%% Asking saving folder and name
 if exist('savepathname', 'var')
    [SaveDataname, savepathname] = uiputfile({'*.mat';'*.*'}, 'Saving measurement',savepathname);
 else
@@ -14,7 +16,7 @@ end
 if SaveDataname == 0 % if cancel is pressed
    error('Cancelled')
 end
-%% Tallennetaan datat .mat tiedostoon
+%% Saving data to .mat file
 save([savepathname,SaveDataname],'t_','phiM_','ampM_','pO2M_')
 
 
